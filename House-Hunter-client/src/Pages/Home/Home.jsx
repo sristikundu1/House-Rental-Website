669612da75e5ec7ebad05aa4da85b849c60f5bf7
sristikundu1 <nidhukundu1234@gmail.com>
2024-01-24 03:30:05 +0600

    show all houses in the homepage

--- House-Hunter-client/src/Pages/Home/Home.jsx
+++ House-Hunter-client/src/Pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import HouseCard from "./HouseCard";
 const Home = () => {
     const houses = useLoaderData();
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        <div className="mx-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
            {
             houses?.map((allhouse) => <HouseCard
 
--- House-Hunter-client/src/Pages/Home/HouseCard.jsx
+++ House-Hunter-client/src/Pages/Home/HouseCard.jsx
@@ -1,9 +1,19 @@
 
 
-const HouseCard = () => {
+const HouseCard = ({ allhouse }) => {
+    const { _id, title, image, rent } = allhouse;
     return (
         <div>
-            
+
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure><img className="w-full h-64 object-cover" src={image} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{title}</h2>
+                    <p>Rent: {rent}</p>
+                   
+                </div>
+            </div>
+
         </div>
     );
 };
